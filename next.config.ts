c5c4b33b64934b83dd8b2207d29d21e458161695
Aastha Porwal <aasthaporwal47@gmail.com>
2026-03-22 11:35:53 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://vadilal

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'vadilalglobal.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };

1f3e148e401c0dc07a004511a0bf4541edc5e000
aastha259 <balramporwal47@gmail.com>
2026-04-03 19:30:41 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-3747808795-e1fe5",
-  "appId": "1:832259040213:web:996b2a23a2462d9eae135c",
-  "apiKey": "AIzaSyBPfjHSlK6-BDa1_GickhXA68f6CWyRD-o",
-  "authDomain": "studio-3747808795-e1fe5.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "832259040213"
+  apiKey: "AIzaSyDJhSSTcOgYHt9y_bJEYn4ih6D6_4Rht4s",
+  authDomain: "bhartiya-swad-1.firebaseapp.com",
+  projectId: "bhartiya-swad-1",
+  storageBucket: "bhartiya-swad-1.firebasestorage.app",
+  messagingSenderId: "219231093103",
+  appId: "1:219231093103:web:9b37d8e1232bfb26a1e89a",
+  measurementId: "G-PLK8QX4075"
 };
